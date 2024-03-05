12c8f8726140e2744e7f442b684fefb45d9a5967
Gezsh <teramedewgezhagn@gmail.com>
2024-03-05 17:37:11 +0300

    deleting port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URL)
         .then(()=>console.log("Database is connected"))
         .catch((err)=>console.log(err))
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(3000, ()=>{
 
     console.log("Backend server is running")
 })
